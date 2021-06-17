8bd8f15970f1ef435da9bfe8a2d92edb8d6525df
えいじ <86034356+Eiji-Itaya@users.noreply.github.com>
2021-06-18 01:34:17 +0900

    initial commit
    サンプルプログラムその2

---
+++ sample(1)/index.js
@@ -0,0 +1,97 @@
+function init() {
+        // サイズを指定
+        const width = 960;
+        const height = 540;
+        let rot = 0; // 角度
+
+        // レンダラーを作成
+        const renderer = new THREE.WebGLRenderer({
+          canvas: document.querySelector('#myCanvas')
+        });
+        renderer.setPixelRatio(window.devicePixelRatio);
+        renderer.setSize(width, height);
+
+        // シーンを作成
+        const scene = new THREE.Scene();
+
+        // カメラを作成
+        const camera = new THREE.PerspectiveCamera(45, width / height);
+
+        // 星屑を作成します (カメラの動きをわかりやすくするため)
+        createStarField();
+
+        function createStarField() {
+          // 形状データを作成
+            var geometry = new THREE.Geometry();
+
+            var geometry1 = new THREE.SphereGeometry();
+          // マテリアルを作成
+          var material = new THREE.PointsMaterial({
+            // 一つ一つのサイズ
+            size: 10,
+            // 色
+            color: 0xffffff
+          });
+
+            var material1 = new THREE.PointsMaterial({
+                // 一つ一つのサイズ
+                size: 10,
+                // 色
+                color: 0xffff00
+            });
+
+
+          // 配置する範囲
+          const SIZE = 3000;
+          // 配置する個数
+          const LENGTH = 1000;
+
+          for (let i = 0; i < LENGTH; i++) {
+            geometry.vertices.push(
+                new THREE.Vector3(
+                    //Math.random()は0～1までの浮動小数点を返す
+                SIZE * (Math.random() - 0.5),
+                SIZE * (Math.random() - 0.5),
+                SIZE * (Math.random() - 0.5)
+             )
+            );
+
+              geometry1.vertices.push(
+                  new THREE.Vector3(
+                      //Math.random()は0～1までの浮動小数点を返す
+                      SIZE * (Math.random() - 0.5),
+                      SIZE * (Math.random() - 0.5),
+                      SIZE * (Math.random() - 0.5)
+                  )
+              );
+          }
+            // 物体を作成
+            const mesh1 = new THREE.Points(geometry1, material1);
+            scene.add(mesh1);
+            
+
+          // 物体を作成
+          const mesh = new THREE.Points(geometry, material);
+          scene.add(mesh);
+        }
+
+   
+        tick();
+
+        // 毎フレーム時に実行されるループイベントです
+        function tick() {
+          rot += 1;
+          // ラジアンに変換する
+          const radian = (rot * Math.PI) / 180;
+          // 角度に応じてカメラの位置を設定
+          camera.position.x = 1000 * Math.sin(radian);
+          camera.position.z = 1000 * Math.cos(radian);
+          // 原点方向を見つめる
+          camera.lookAt(new THREE.Vector3(0, 0, 0));
+          // レンダリング
+          renderer.render(scene, camera);
+          requestAnimationFrame(tick);
+        }
+}
+
+window.addEventListener('DOMContentLoaded', init);
